9f16efdece652a30a38b6f5da965a6f7b0d62544
taozhi.chao <taozhi.chao@united-imaging.com>
2024-10-10 13:19:12 +0800

    发布

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/site/dist/',
+  // base: '/site/docs/',
   base: '/AI-Page1/',
   plugins: [react()],
+  build: {
+    outDir: "docs"
+  }
 })
